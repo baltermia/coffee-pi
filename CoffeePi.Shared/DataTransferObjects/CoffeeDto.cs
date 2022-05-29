c74eea63fb23335ef69861dda557d3cc47a788e1
Mawule Adzayi <mawule.adzayi@inventx.ch>
2022-05-29 16:40:43 +0200

    Expanded CoffeeDto with Coffee Type and Removed Timed Dto

--- CoffeePi.Shared/DataTransferObjects/CoffeeDto.cs
+++ CoffeePi.Shared/DataTransferObjects/CoffeeDto.cs
@@ -4,5 +4,6 @@ public class CoffeeDto
 {
     public int Id { get; init; }
     public DateTime Time { get; set; }
-    public int RepeatValue { get; set; }
+    public int RepeatValue { get; set; } // ToDo: set values for Daily, weelky repeats
+    public string CoffeeType { get; set; }
 }
