a2fffa30c2623bdc00486f31f0267182e736dba7
baltermia <54800864+baltermia@users.noreply.github.com>
2022-05-29 21:48:15 +0200

    Fix Database project reference in Core

--- CoffeePi.Core/Program.cs
+++ CoffeePi.Core/Program.cs
@@ -1,5 +1,5 @@
 using CoffeePi.Core.Services;
-using CoffeePi.Shared.Configuration;
+using CoffeePi.Database.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
