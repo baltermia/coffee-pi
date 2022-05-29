516c4ffd39298da669927f90b8272a6ce6c8f06b
baltermia <54800864+baltermia@users.noreply.github.com>
2022-05-29 20:07:33 +0200

    Add PlannedRoutine model and initial ef migration

--- CoffeePi.Shared/Enums/CoffeeButton.cs
+++ CoffeePi.Shared/Enums/CoffeeButton.cs
@@ -1,9 +1,11 @@
-﻿namespace CoffeePi.Core.Services;
+﻿namespace CoffeePi.Shared.Enums;
 
 /// <summary>
 /// Enum representing all buttons that can be simulated from the raspberry pi
+/// 
+/// The int Values represent the GPIO pin on the raspberry pi
 /// </summary>
-public enum CoffeeButtonPins
+public enum CoffeeButton
 {
     SmallCup = 11,
     BigCup = 17,
---
+++ CoffeePi.Shared/Models/PlannedRoutine.cs
@@ -0,0 +1,20 @@
+﻿using CoffeePi.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeePi.Shared.Models;
+
+/// <summary>
+/// Model representing a planned routine for a button press
+/// </summary>
+public class PlannedRoutine
+{
+    public int Id { get; init; }
+
+    [Required]
+    public CoffeeButton Button { get; set; }
+
+    [Required]
+    public DateTime Time { get; set; }
+
+    public bool? Successful = null;
+}
