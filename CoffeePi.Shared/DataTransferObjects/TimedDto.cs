8414822a68d5830015e42eb494b106c75fc5303d
baltermia <54800864+baltermia@users.noreply.github.com>
2022-05-29 15:00:29 +0200

    Add Web project

---
+++ CoffeePi.Shared/DataTransferObjects/TimedDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CoffeePi.Shared.DataTransferObjects;
+
+public class TimedDto
+{
+    public int Id { get; init; }
+    public DateTime Time { get; set; }
+    public bool Repeating  { get; set; }
+}
---
+++ CoffeePi.Web/Program.cs
@@ -0,0 +1,17 @@
+using CoffeePi.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "api/") });
+//if (builder.HostEnvironment.IsDevelopment())
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://dummyapi.io/data/v1/") });
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
