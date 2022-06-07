60b72aab092803f715b718ceccb6e36963f1225f
speyck <54800864+baltermia@users.noreply.github.com>
2022-06-07 11:36:05 +0200

    Add extension mapping methods for all Data Models/DTOs

---
+++ CoffeePi.Data/Mappings/CoffeeRoutineMappings.cs
@@ -0,0 +1,50 @@
+﻿using CoffeePi.Data.Models;
+using CoffeePi.Shared.DataTransferObjects;
+
+namespace CoffeePi.Data.Mappings;
+
+public static class CoffeeRoutineMappings
+{
+    public static CoffeeRoutineDto ToDto(this CoffeeRoutine routine) => routine switch
+    {
+        SingleRoutine single => single.ToDto(),
+        WeeklyRoutine weekly => weekly.ToDto(),
+        DailyRoutine daily => daily.ToDto(),
+        _ => throw new ArgumentException($"CoffeeRoutine is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToDto)}")
+    };
+
+    public static SingleRoutineDto ToDto(this SingleRoutine routine) =>
+        routine == default(SingleRoutine) ?
+            default :
+            new SingleRoutineDto
+            {
+                Id = routine.Id,
+                ButtonType = routine.ButtonType,
+                Time = routine.Time,
+                ExecutionId = routine.Execution.Id
+            };
+
+    public static WeeklyRoutineDto ToDto(this WeeklyRoutine routine) =>
+        routine == default(WeeklyRoutine) ?
+            default :
+            new WeeklyRoutineDto
+            {
+                Id = routine.Id,
+                ButtonType = routine.ButtonType,
+                DayOfWeek = routine.DayOfWeek,
+                TimeOfDay = routine.TimeOfDay,
+                ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
+            };
+
+    public static DailyRoutineDto ToDto(this DailyRoutine routine) =>
+        routine == default(DailyRoutine) ?
+            default :
+            new DailyRoutineDto
+            {
+                Id = routine.Id,
+                ButtonType = routine.ButtonType,
+                DaysOfWeek = routine.DaysOfWeek,
+                TimeOfDay = routine.TimeOfDay,
+                ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
+            };
+}
---
+++ CoffeePi.Data/Mappings/ExecutedRoutineMappings.cs
@@ -0,0 +1,18 @@
+﻿using CoffeePi.Data.Models;
+using CoffeePi.Shared.DataTransferObjects;
+
+namespace CoffeePi.Data.Mappings;
+
+public static class ExecutedRoutineMappings
+{
+    public static ExecutedRoutineDto ToDto(this ExecutedRoutine execution) =>
+        execution == default(ExecutedRoutine) ?
+            default :
+            new ExecutedRoutineDto
+            {
+                Id = execution.Id,
+                RoutineId = execution.Routine.Id,
+                Time = execution.Time,
+                Success = execution.Success
+            };
+}
