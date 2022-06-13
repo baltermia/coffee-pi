bcbd92e7c97c1ac7d3f8048f03734f273f6b475a
Mawule Adzayi <mawule.adzayi@inventx.ch>
2022-06-13 16:32:22 +0200

    Remove: unneeded files

--- CoffeePi.Shared/DataTransferObjects/CoffeeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoffeePi.Shared.DataTransferObjects;
-
-public class CoffeeDto
-{
-    public int Id { get; init; }
-    public DateTime Time { get; set; }
-    public int RepeatValue { get; set; }
-}
