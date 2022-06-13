b8c23d7471a0a7d0ea59538d0cde1c0fd52832fd
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-13 21:25:53 +0200

    Add reverse mappings

--- CoffeePi.Data/Mappings/CoffeeRoutineMappings.cs
+++ CoffeePi.Data/Mappings/CoffeeRoutineMappings.cs
@@ -13,6 +13,14 @@ public static class CoffeeRoutineMappings
         _ => throw new ArgumentException($"CoffeeRoutine is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToDto)}")
     };
 
+    public static CoffeeRoutine ToModel(this CoffeeRoutineDto dto, CoffeeRoutine routine = null) => dto switch
+    {
+        SingleRoutineDto single => single.ToModel(routine as SingleRoutine),
+        WeeklyRoutineDto weekly => weekly.ToModel(routine as WeeklyRoutine),
+        DailyRoutineDto daily => daily.ToModel(routine as DailyRoutine),
+        _ => throw new ArgumentException($"CoffeeRoutineDto is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToModel)}")
+    };
+
     public static SingleRoutineDto ToDto(this SingleRoutine routine) =>
         routine == default(SingleRoutine) ?
             default :
@@ -23,6 +31,16 @@ public static class CoffeeRoutineMappings
                 Time = routine.Time,
                 ExecutionId = routine.Execution.Id
             };
+
+    public static SingleRoutine ToModel(this SingleRoutineDto dto, SingleRoutine routine = null)
+    {
+        routine ??= new();
+
+        routine.ButtonType = dto.ButtonType;
+        routine.Time = dto.Time;
+
+        return routine;
+    }
 
     public static WeeklyRoutineDto ToDto(this WeeklyRoutine routine) =>
         routine == default(WeeklyRoutine) ?
@@ -36,6 +54,17 @@ public static class CoffeeRoutineMappings
                 ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
             };
 
+    public static WeeklyRoutine ToModel(this WeeklyRoutineDto dto, WeeklyRoutine routine = null)
+    {
+        routine ??= new();
+
+        routine.ButtonType = dto.ButtonType;
+        routine.TimeOfDay = dto.TimeOfDay;
+        routine.DayOfWeek = dto.DayOfWeek;
+
+        return routine;
+    }
+
     public static DailyRoutineDto ToDto(this DailyRoutine routine) =>
         routine == default(DailyRoutine) ?
             default :
@@ -47,4 +76,15 @@ public static class CoffeeRoutineMappings
                 TimeOfDay = routine.TimeOfDay,
                 ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
             };
+
+    public static DailyRoutine ToModel(this DailyRoutineDto dto, DailyRoutine routine = null)
+    {
+        routine ??= new();
+
+        routine.ButtonType = dto.ButtonType;
+        routine.TimeOfDay = dto.TimeOfDay;
+        routine.DaysOfWeek = dto.DaysOfWeek;
+
+        return routine;
+    }
 }
