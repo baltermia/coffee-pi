c3ee0db7225e123f0828353c3a4364b5dea89357
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-13 22:14:07 +0200

    Add IRepositoryBase interface

---
+++ CoffeePi.Core/Repositories/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using CoffeePi.Shared.DataTransferObjects;
+
+namespace CoffeePi.Core.Repositories
+{
+    public interface IRepositoryBase<T> where T : IDataTransferObject
+    {
+        public IEnumerable<T> FindAll();
+
+        public T FindById(int id);
+
+        public Task<T> CreateAsync(T dto);
+
+        public Task UpdateAsync(T dto);
+    }
+}
