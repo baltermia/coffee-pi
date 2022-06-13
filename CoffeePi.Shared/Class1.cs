a09e3fba81c2703a1f088cb45871b05f07fb1ca5
Mawule Adzayi <mawule.adzayi@inventx.ch>
2022-06-13 16:27:26 +0200

    Add: Base Frontend functionality

---
+++ CoffeePi.Shared/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeePi.Shared
+{
+    internal class Class1
+    {
+    }
+}
---
+++ CoffeePi.Shared/DataTransferObjects/CoffeeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CoffeePi.Shared.DataTransferObjects;
+
+public class CoffeeDto
+{
+    public int Id { get; init; }
+    public DateTime Time { get; set; }
+    public int RepeatValue { get; set; }
+}
