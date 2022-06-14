d92a8f2fd7222ab1f05a1972dec6dc2c6913f95c
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 13:10:55 +0200

    Add GpioController

---
+++ CoffeePi.Core/Controllers/GpioController.cs
@@ -0,0 +1,33 @@
+﻿using CoffeePi.Core.Services;
+using CoffeePi.Shared.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeePi.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GpioController : Controller
+    {
+        private readonly IGpioService _gpioService;
+
+        public GpioController(IGpioService gpioService)
+        {
+            _gpioService = gpioService;
+        }
+
+        [HttpPost("simulate")]
+        public ActionResult SimulateButton(CoffeeButton button)
+        {
+            try
+            {
+                _gpioService.SimulatePress(button);
+
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error simulating button press");
+            }
+        }
+    }
+}
