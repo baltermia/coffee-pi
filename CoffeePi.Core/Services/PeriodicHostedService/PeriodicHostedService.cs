4365e674735bd716f998bbf77f51c7f464605619
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 14:21:06 +0200

    Rename routine service methods

--- CoffeePi.Core/Services/PeriodicHostedService/PeriodicHostedService.cs
+++ CoffeePi.Core/Services/PeriodicHostedService/PeriodicHostedService.cs
@@ -16,7 +16,7 @@ public class PeriodicHostedService : BackgroundService, IPeriodicHostedService
 
     protected async override Task ExecuteAsync(CancellationToken token)
     {
-        using PeriodicTimer timer = new(period);
+        using PeriodicTimer timer = new (period);
 
         while (!token.IsCancellationRequested && await timer.WaitForNextTickAsync(token))
         {
@@ -31,7 +31,7 @@ public class PeriodicHostedService : BackgroundService, IPeriodicHostedService
 
                 IRoutineService routineService = asyncScope.ServiceProvider.GetRequiredService<IRoutineService>();
 
-                await routineService.DoRoutineWorkAsync(token);
+                await routineService.CheckAllRoutinesAsync(token);
             }
             catch { } // TODO: Add exception logging (perhaps?)
         }
