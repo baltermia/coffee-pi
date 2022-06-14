7af1ee3fcd04607ff38599fd68c4f37e216d136a
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 08:59:25 +0200

    Add docs in CoffeRoutineMappings

--- CoffeePi.Data/Mappings/CoffeeRoutineMappings.cs
+++ CoffeePi.Data/Mappings/CoffeeRoutineMappings.cs
@@ -3,22 +3,18 @@ using CoffeePi.Shared.DataTransferObjects;
 
 namespace CoffeePi.Data.Mappings;
 
+/// <summary>
+/// Extension methods to convert Database Models to DataTransferObjects and vice-versa
+/// </summary>
 public static class CoffeeRoutineMappings
 {
+    #region ToDto
     public static CoffeeRoutineDto ToDto(this CoffeeRoutine routine) => routine switch
     {
         SingleRoutine single => single.ToDto(),
         WeeklyRoutine weekly => weekly.ToDto(),
         DailyRoutine daily => daily.ToDto(),
         _ => throw new ArgumentException($"CoffeeRoutine is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToDto)}")
-    };
-
-    public static CoffeeRoutine ToModel(this CoffeeRoutineDto dto, CoffeeRoutine routine = null) => dto switch
-    {
-        SingleRoutineDto single => single.ToModel(routine as SingleRoutine),
-        WeeklyRoutineDto weekly => weekly.ToModel(routine as WeeklyRoutine),
-        DailyRoutineDto daily => daily.ToModel(routine as DailyRoutine),
-        _ => throw new ArgumentException($"CoffeeRoutineDto is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToModel)}")
     };
 
     public static SingleRoutineDto ToDto(this SingleRoutine routine) =>
@@ -32,16 +28,6 @@ public static class CoffeeRoutineMappings
                 ExecutionId = routine.Execution.Id
             };
 
-    public static SingleRoutine ToModel(this SingleRoutineDto dto, SingleRoutine routine = null)
-    {
-        routine ??= new();
-
-        routine.ButtonType = dto.ButtonType;
-        routine.Time = dto.Time;
-
-        return routine;
-    }
-
     public static WeeklyRoutineDto ToDto(this WeeklyRoutine routine) =>
         routine == default(WeeklyRoutine) ?
             default :
@@ -54,17 +40,6 @@ public static class CoffeeRoutineMappings
                 ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
             };
 
-    public static WeeklyRoutine ToModel(this WeeklyRoutineDto dto, WeeklyRoutine routine = null)
-    {
-        routine ??= new();
-
-        routine.ButtonType = dto.ButtonType;
-        routine.TimeOfDay = dto.TimeOfDay;
-        routine.DayOfWeek = dto.DayOfWeek;
-
-        return routine;
-    }
-
     public static DailyRoutineDto ToDto(this DailyRoutine routine) =>
         routine == default(DailyRoutine) ?
             default :
@@ -76,6 +51,37 @@ public static class CoffeeRoutineMappings
                 TimeOfDay = routine.TimeOfDay,
                 ExecutionIds = routine.Executions.Select(e => e.Id).ToList()
             };
+    #endregion
+
+    #region ToModel
+    public static CoffeeRoutine ToModel(this CoffeeRoutineDto dto, CoffeeRoutine routine = null) => dto switch
+    {
+        SingleRoutineDto single => single.ToModel(routine as SingleRoutine),
+        WeeklyRoutineDto weekly => weekly.ToModel(routine as WeeklyRoutine),
+        DailyRoutineDto daily => daily.ToModel(routine as DailyRoutine),
+        _ => throw new ArgumentException($"CoffeeRoutineDto is not of any known type. Please add type to switch statement: {nameof(CoffeeRoutineMappings.ToModel)}")
+    };
+
+    public static SingleRoutine ToModel(this SingleRoutineDto dto, SingleRoutine routine = null)
+    {
+        routine ??= new();
+
+        routine.ButtonType = dto.ButtonType;
+        routine.Time = dto.Time;
+
+        return routine;
+    }
+
+    public static WeeklyRoutine ToModel(this WeeklyRoutineDto dto, WeeklyRoutine routine = null)
+    {
+        routine ??= new();
+
+        routine.ButtonType = dto.ButtonType;
+        routine.TimeOfDay = dto.TimeOfDay;
+        routine.DayOfWeek = dto.DayOfWeek;
+
+        return routine;
+    }
 
     public static DailyRoutine ToModel(this DailyRoutineDto dto, DailyRoutine routine = null)
     {
@@ -87,4 +93,5 @@ public static class CoffeeRoutineMappings
 
         return routine;
     }
+    #endregion
 }
