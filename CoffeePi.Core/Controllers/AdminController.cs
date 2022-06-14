62d6b6f9f513cf17dbb617e0765f716b76e35261
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 11:17:10 +0200

    Add AdminController (manages background state)

---
+++ CoffeePi.Core/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using CoffeePi.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeePi.Core.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        private readonly IPeriodicHostedService _service;
+
+        public AdminController(IPeriodicHostedService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("background")]
+        public ActionResult<bool> GetBackgroundState()
+        {
+            try
+            {
+                bool state = _service.Enabled;
+
+                return Ok(state);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error getting background-task state information");
+            }
+        }
+
+        [HttpPatch("background")]
+        public ActionResult PatchBackgroundState(bool state)
+        {
+            try
+            {
+                _service.Enabled = state;
+
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error getting background-task state information");
+            }
+        }
+    }
+}
