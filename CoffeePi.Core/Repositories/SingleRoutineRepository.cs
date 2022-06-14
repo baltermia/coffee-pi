953b3bf07a327a8e3857c101bdc11af5e2fa74f5
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 09:23:41 +0200

    Implement IRepositoryBase methods for SingleRoutine

---
+++ CoffeePi.Core/Repositories/SingleRoutineRepository.cs
@@ -0,0 +1,61 @@
+﻿using CoffeePi.Data.Configuration;
+using CoffeePi.Data.Mappings;
+using CoffeePi.Data.Models;
+using CoffeePi.Shared.DataTransferObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeePi.Core.Repositories
+{
+    public interface ISingleRoutineRepository : IRepositoryBase<SingleRoutineDto> { }
+
+    public class SingleRoutineRepository : ISingleRoutineRepository
+    {
+        private readonly CoffeePiContext _context;
+
+        public SingleRoutineRepository(CoffeePiContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SingleRoutineDto> FindAll() =>
+            _context
+                .Set<CoffeeRoutine>()
+                .OfType<SingleRoutine>()
+                .AsNoTracking()
+                .Select(CoffeeRoutineMappings.ToDto);
+
+        public SingleRoutineDto FindById(int id) =>
+            _context
+                .Set<CoffeeRoutine>()
+                .OfType<SingleRoutine>()
+                .AsNoTracking()
+                .SingleOrDefault(e => e.Id == id)
+                .ToDto();
+
+        public async Task<SingleRoutineDto> CreateAsync(SingleRoutineDto dto)
+        {
+            SingleRoutine routine = dto.ToModel();
+
+            await _context.AddAsync<CoffeeRoutine>(routine);
+
+            await _context.SaveChangesAsync();
+
+            return routine.ToDto();
+        }
+
+        public async Task UpdateAsync(SingleRoutineDto dto)
+        {
+            SingleRoutine routine =
+                _context
+                    .Set<CoffeeRoutine>()
+                    .OfType<SingleRoutine>()
+                    .Single(e => e.Id == dto.Id);
+
+            routine = dto.ToModel(routine);
+
+            _context.Update<CoffeeRoutine>(routine);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
