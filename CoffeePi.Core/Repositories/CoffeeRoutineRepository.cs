be0d043ee835e22ac6c2a78a25a7d3d08f8482be
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 09:39:12 +0200

    Implement IRepositoryBase methods for CoffeeRoutine

---
+++ CoffeePi.Core/Repositories/CoffeeRoutineRepository.cs
@@ -0,0 +1,55 @@
+﻿using CoffeePi.Data.Configuration;
+using CoffeePi.Data.Mappings;
+using CoffeePi.Data.Models;
+using CoffeePi.Shared.DataTransferObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeePi.Core.Repositories
+{
+    public interface ICoffeeRoutineRepository : IRepositoryBase<CoffeeRoutineDto> { }
+
+    public class CoffeeRoutineRepository : ICoffeeRoutineRepository
+    {
+        private readonly CoffeePiContext _context;
+        private readonly ISingleRoutineRepository _singleRepo;
+        private readonly IWeeklyRoutineRepository _weeklyRepo;
+        private readonly IDailyRoutineRepository _dailyRepo;
+
+        public CoffeeRoutineRepository(CoffeePiContext context, ISingleRoutineRepository singleRepo, IWeeklyRoutineRepository weeklyRepo, IDailyRoutineRepository dailyRepo)
+        {
+            _context = context;
+            _singleRepo = singleRepo;
+            _weeklyRepo = weeklyRepo;
+            _dailyRepo = dailyRepo;
+        }
+
+        public IEnumerable<CoffeeRoutineDto> FindAll() =>
+            _context
+                .Set<CoffeeRoutine>()
+                .AsNoTracking()
+                .Select(CoffeeRoutineMappings.ToDto);
+
+        public CoffeeRoutineDto FindById(int id) =>
+            _context
+                .Set<CoffeeRoutine>()
+                .AsNoTracking()
+                .SingleOrDefault(e => e.Id == id)
+                .ToDto();
+
+        public async Task<CoffeeRoutineDto> CreateAsync(CoffeeRoutineDto dto) => dto switch
+        {
+            SingleRoutineDto single => await _singleRepo.CreateAsync(single),
+            WeeklyRoutineDto weekly => await _weeklyRepo.CreateAsync(weekly),
+            DailyRoutineDto daily => await _dailyRepo.CreateAsync(daily),
+            _ => throw CoffeeRoutineMappings.DtoMappingNotFound
+        };
+
+        public Task UpdateAsync(CoffeeRoutineDto dto) => dto switch
+        {
+            SingleRoutineDto single => _singleRepo.UpdateAsync(single),
+            WeeklyRoutineDto weekly => _weeklyRepo.UpdateAsync(weekly),
+            DailyRoutineDto daily => _dailyRepo.UpdateAsync(daily),
+            _ => throw CoffeeRoutineMappings.DtoMappingNotFound
+        };
+    }
+}
