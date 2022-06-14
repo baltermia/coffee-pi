558c59aea1bf189ed616746b049bce79340c7b31
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-14 09:46:07 +0200

    Implement IRepositoryBase methods for ExecutedRoutine

---
+++ CoffeePi.Core/Repositories/ExecutedRoutineRepository.cs
@@ -0,0 +1,58 @@
+﻿using CoffeePi.Data.Configuration;
+using CoffeePi.Data.Mappings;
+using CoffeePi.Data.Models;
+using CoffeePi.Shared.DataTransferObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeePi.Core.Repositories
+{
+    public interface IExecutedRoutineRepository : IRepositoryBase<ExecutedRoutineDto> { }
+
+    public class ExecutedRoutineRepository : IExecutedRoutineRepository
+    {
+        private readonly CoffeePiContext _context;
+
+        public ExecutedRoutineRepository(CoffeePiContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ExecutedRoutineDto> FindAll() =>
+            _context
+                .Set<ExecutedRoutine>()
+                .AsNoTracking()
+                .Select(ExecutedRoutineMappings.ToDto);
+
+        public ExecutedRoutineDto FindById(int id) =>
+            _context
+                .Set<ExecutedRoutine>()
+                .AsNoTracking()
+                .SingleOrDefault(e => e.Id == id)
+                .ToDto();
+
+        public async Task<ExecutedRoutineDto> CreateAsync(ExecutedRoutineDto dto)
+        {
+            ExecutedRoutine routine = dto.ToModel();
+
+            await _context.AddAsync(routine);
+
+            await _context.SaveChangesAsync();
+
+            return routine.ToDto();
+        }
+
+        public async Task UpdateAsync(ExecutedRoutineDto dto)
+        {
+            ExecutedRoutine routine =
+                _context
+                    .Set<ExecutedRoutine>()
+                    .Single(e => e.Id == dto.Id);
+
+            routine = dto.ToModel(routine);
+
+            _context.Update(routine);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
