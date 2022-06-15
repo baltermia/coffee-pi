448e173ae045705e1443f730fc39144d34a9da94
baltermia <54800864+baltermia@users.noreply.github.com>
2022-06-15 09:35:57 +0200

    Add HttpExtensions to use TypenameHandling

---
+++ CoffeePi.Shared/Utils/HttpExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace CoffeePi.Shared.Utils;
+
+public static class HttpExtensions
+{
+    private static readonly JsonSerializerSettings settings = new()
+    {
+        TypeNameHandling = TypeNameHandling.Auto
+    };
+
+    public static async Task<HttpResponseMessage> PostBasicAsync<T>(this HttpClient client, string url, T value)
+    {
+        string json = JsonConvert.SerializeObject(value, settings);
+
+        StringContent content = GetContent(json);
+
+        HttpResponseMessage result = await client.PostAsync(url, content);
+
+        return result;
+    }
+
+    public static async Task<HttpResponseMessage> PutBasicAsync<T>(this HttpClient client, string url, T value)
+    {
+        string json = JsonConvert.SerializeObject(value, settings);
+
+        StringContent content = GetContent(json);
+
+        HttpResponseMessage result = await client.PutAsync(url, content);
+
+        return result;
+    }
+
+    public static async Task<T> DeserializeResponse<T>(this HttpResponseMessage response)
+    {
+        string json = await response.Content.ReadAsStringAsync();
+
+        T value = JsonConvert.DeserializeObject<T>(json, settings);
+
+        return value;
+    }
+
+    private static StringContent GetContent(string json) => new(json, Encoding.UTF8, "application/json");
+}
