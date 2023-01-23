56a4620f41d1bab47e92b4cd66d9c94cac73ee75
baltermia <54800864+baltermia@users.noreply.github.com>
2023-01-23 14:06:56 +0100

    Add Migration for newly added property

---
+++ CoffeePi.Data/Migrations/20230123130557_AddMachinePropertiesKey.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeePi.Data.Migrations
+{
+    public partial class AddMachinePropertiesKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "MachineProperties",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MachineProperties",
+                table: "MachineProperties",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MachineProperties",
+                table: "MachineProperties");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "MachineProperties");
+        }
+    }
+}
